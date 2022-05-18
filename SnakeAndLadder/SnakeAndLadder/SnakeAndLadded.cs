1480b680da28709b91829da7d7b78fef28d1fc6f
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-18 08:34:33 +0530

    [Nanth ] - Noplay,Snake and Ladder added

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadded.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadded.cs
@@ -8,40 +8,29 @@ namespace SnakeAndLadder
 {
     public class snakeGame
     {
-        int player1 = 0,diceCount=0;
+        int player1 = 0;
         public void startGame()
         {
-            for (player1 = 0; player1 < 100;)
+
+            Random dice = new Random();
+            int diceValue = dice.Next(1, 7);
+            Console.WriteLine(diceValue);
+            int possibility = dice.Next(3);
+            switch (possibility)
             {
-                Random dice = new Random();
-                int diceValue = dice.Next(1, 7);
-                if (player1 < 100 && player1 >= 0)
-                {
-                    Console.WriteLine(diceValue);
-                    int possibility = dice.Next(3);
-                    switch (possibility)
-                    {
-                        case 1: // ladder
-                            player1 = player1 + diceValue;
-                            break;
-                        case 2: // snake
-                            player1 = player1 - diceValue;
-                            break;
-                        case 0: // noPlay
-                            break;
-                    }
-                    Console.WriteLine(player1);
-                    if(player1>100)
-                    {
-                        player1 = player1 - diceValue;
-                    }
-                }
-                else { player1 = 0; }
-                diceCount++;
-                
+                case 1: // ladder
+                    player1 = player1 + diceValue;
+                    break;
+                case 2: // snake
+                    player1 = player1 - diceValue;
+                    break;
+                case 0: // noPlay
+                    break;
             }
-            Console.WriteLine("Number of times dice rolled = " + diceCount);
+            Console.WriteLine(player1);
+
+
         }
-        
     }
 }
+
