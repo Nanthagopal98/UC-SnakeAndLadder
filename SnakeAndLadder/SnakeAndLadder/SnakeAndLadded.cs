1379afadff48c15a28c2ca9d02027bd25a06cd8f
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-18 12:53:45 +0530

    [Nantha] - compared and prit the winner

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadded.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadded.cs
@@ -8,40 +8,89 @@ namespace SnakeAndLadder
 {
     public class snakeGame
     {
-        int player1 = 0,diceCount=0;
+        int player1 = 0, player2 = 0, player1DiceCount = 0, player2DiceCount = 0, selectPlayer = 1;
+        Random dice = new Random();
         public void startGame()
         {
-            for (player1 = 0; player1 < 100;)
+            while (player2 < 100 && player1 < 100)
             {
-                Random dice = new Random();
-                int diceValue = dice.Next(1, 7);
-                if (player1 < 100 && player1 >= 0)
+                int possibility = dice.Next(3);
+                if (selectPlayer == 1)
                 {
-                    Console.WriteLine("Dice value = " + diceValue);
-                    int possibility = dice.Next(3);
-                    switch (possibility)
+                    int diceValue = dice.Next(1, 7);
+                    if (player1 < 100 && player1 >= 0)
                     {
-                        case 1: // ladder
-                            player1 = player1 + diceValue;
-                            break;
-                        case 2: // snake
-                            player1 = player1 - diceValue;
-                            break;
-                        case 0: // noPlay
-                            break;
+                        Console.WriteLine("Player1 Dice value = " + diceValue);
+                        switch (possibility)
+                        {
+                            case 1: // ladder
+                                player1 = player1 + diceValue;
+                                selectPlayer = 1;
+                                if (player1 > 100)
+                                {
+                                    player1 = player1 - diceValue;
+                                }
+                                break;
+                            case 2: // snake
+                                player1 = player1 - diceValue;
+                                selectPlayer = 2;
+                                break;
+                            case 0: // noPlay
+                                break;
+                        }
+                        Console.WriteLine("Player1 current position = " + player1);
+                        if (player1 < 0)
+                        {
+                            player1 = 0;
+                        }
                     }
-                    Console.WriteLine("Player current position = " + player1);
-                    if(player1>100)
-                    {
-                        player1 = player1 - diceValue;
-                    }
+                    player1DiceCount++;
                 }
-                else if (player1 < 0)
-                { player1 = 0; }
-                diceCount++; 
+                else
+                {
+                    int diceValue = dice.Next(1, 7);
+                    if (player2 < 100 && player2 >= 0)
+                    {
+                        Console.WriteLine("player2 Dice value = " + diceValue);
+                        switch (possibility)
+                        {
+                            case 1: // ladder
+                                player2 = player2 + diceValue;
+                                selectPlayer = 2;
+                                if (player2 > 100)
+                                {
+                                    player2 = player2 - diceValue;
+                                }
+                                    break;
+                            case 2: // snake
+                                player2 = player2 - diceValue;
+                                selectPlayer = 1;
+                                break;
+                            case 0: // noPlay
+                                break;
+                        }
+                        Console.WriteLine("Player2 current position = " + player2);
+                    }
+                        if (player2 < 0)
+                        {
+                            player2 = 0;
+                        }
+                    
+                    player2DiceCount++;
+                } 
             }
-            Console.WriteLine("Number of times dice rolled = " + diceCount);
+            Console.WriteLine();
+            Console.WriteLine("Player1 dice rolled count = " + player1DiceCount);
+            Console.WriteLine("Player2 dice rolled count = " + player2DiceCount);
+            Console.WriteLine("Total dice Count = " + (player2DiceCount+ player1DiceCount));
+            if (player1 == 100)
+            {
+                Console.WriteLine("Player1 Wins");
+            }
+            else
+            {
+                Console.WriteLine("player2 wins");
+            }
         }
-        
     }
 }
